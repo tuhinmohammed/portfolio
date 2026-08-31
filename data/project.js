53f58ba27868ec006d84a1f8d02bfbcb978f535f
Mohammed Monirul Islam Tuhin <158435148+tuhinmohammed@users.noreply.github.com>
2026-08-31 02:16:54 -0500

    Update project.js

--- data/project.js
+++ data/project.js
@@ -3,8 +3,7 @@ export const projects = [
     title: "Bank Marketing Model",
     year: "2026",
     status: "In Progress",
-    description: "Python data science project analyzing Portuguese bank telemarketing data and predicting term-deposit subscriptions using classification models.
-",
+    description: "Python data science project analyzing Portuguese bank telemarketing data and predicting term-deposit subscriptions using classification models.",
     technologies: ["Python", "Numpy", "Pandas", "Matplotlib","Jupyter Notebook"],
     link: "https://github.com/tuhinmohammed/bank-marketing-subscription-prediction.git",
   },
